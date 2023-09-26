9fc10595512a9a44590f2b19f82a36947a44f6e8
Tanix <tanixlu@qq.com>
2023-09-26 22:12:10 +0800

    set task via wlt_task.vbs

--- src/task.rs
+++ src/task.rs
@@ -1,8 +1,12 @@
-use std::process::{Command, Output};
+use std::{
+    path::PathBuf,
+    process::{Command, Output},
+};
 
 use crate::utils::AnyResult;
 
 const TASK_NAME: &str = "wlt_task";
+const VBS_NAME: &str = "wlt_task.vbs";
 
 fn output_string(mut output: Output) -> AnyResult<String> {
     let mut buf = output.stdout;
@@ -10,11 +14,32 @@ fn output_string(mut output: Output) -> AnyResult<String> {
     Ok(String::from_utf8(buf)?)
 }
 
+fn make_task_vbs_file() -> AnyResult<()> {
+    let current_exe = std::env::current_exe()?;
+    let contents = format!(
+        r#"Set wShell = CreateObject("WScript.Shell")
+wShell.Run "cmd /c {}", 0
+"#,
+        current_exe.to_string_lossy()
+    );
+    std::fs::write(VBS_NAME, contents)?;
+
+    Ok(())
+}
+
 pub fn set_task() -> AnyResult<String> {
+    make_task_vbs_file()?;
+    let wscript_path = PathBuf::new()
+        .join(std::env::var("WINDIR")?)
+        .join("System32")
+        .join("wscript.exe");
+    let current_dir = std::env::current_dir()?;
+    let vbs_path = current_dir.join(VBS_NAME);
     let action = format!(
-        "(New-ScheduledTaskAction -Execute {} -WorkingDirectory {})",
-        std::env::current_exe()?.to_string_lossy(),
-        std::env::current_dir()?.to_string_lossy()
+        "(New-ScheduledTaskAction -Execute {} -WorkingDirectory {} -Argument {})",
+        wscript_path.to_string_lossy(),
+        current_dir.to_string_lossy(),
+        vbs_path.to_string_lossy()
     );
     const DESCRIPTION: &str = r#""Configure WLT and send notification emails when the IP changes""#;
     const SETTINGS: &str = "(New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -RunOnlyIfNetworkAvailable)";
@@ -58,4 +83,9 @@ pub fn query_task() -> AnyResult<String> {
         .arg("-Verbose")
         .output()?;
     output_string(output)
+}
+
+#[test]
+fn t() {
+    dbg!(std::env::var("WINDIR").unwrap());
 }
